fbe0fc2f5c8d7ce8649b7560f45648a9948f781d
Leonardo Nava <L3oNav@outlook.com>
2023-12-14 14:01:49 -0800

    whisper inicialization with the queue.

---
+++ app/whisper/manager.py
@@ -0,0 +1,56 @@
+from app.utils.manager import Manager
+class WhisperManager(Manager):
+    def __init__(self, api_key):
+        super().__init__()
+        self.api_key = api_key
+
+    def process_audio(self, audio_file_path):
+        try:
+            # Load the Whisper model (assuming it's pre-loaded)
+            model = whisper.load_model("base")
+
+            # Load audio from the given file and pad/trim it
+            audio = whisper.load_audio(audio_file_path)
+            audio = whisper.pad_or_trim(audio)
+
+            # Make a log-Mel spectrogram and move it to the model's device
+            mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+            # Detect the spoken language
+            _, probs = model.detect_language(mel)
+            detected_language = max(probs, key=probs.get)
+
+            # Decode the audio
+            options = whisper.DecodingOptions()
+            result = whisper.decode(model, mel, options)
+
+            # Process the recognized text or language as needed
+            recognized_text = result.text
+
+            # For demonstration, we print the detected language and recognized text
+            print(f"Detected language: {detected_language}")
+            print(f"Recognized text: {recognized_text}")
+
+            # Implement further processing or storage of the recognized text
+            # ...
+
+            return recognized_text
+        except Exception as e:
+            raise Exception(f"Error processing audio: {str(e)}")
+
+    def remove_processed_audio(self, audio_file_path):
+        # Implement logic to remove or archive processed audio files
+        pass
+
+    def run_processing_loop(self):
+        while not self.audio_queue.empty():
+            audio_file_path = self.audio_queue.get()
+            
+            try:
+                transcription = self.process_audio(audio_file_path)
+                # Remove the processed audio file from the queue
+                self.remove_processed_audio(audio_file_path)
+                
+            except Exception as e:
+                # Handle errors and retries here
+                print(f"Error processing audio file {audio_file_path}: {str(e)}")
